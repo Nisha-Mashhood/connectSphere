3523ac0a701ed8da6944edcf833c6ccd67f0ec66
nishamashhood <mashhoodnisha@gmail.com>
2024-12-19 15:44:11 +0530

    react persistant included

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
