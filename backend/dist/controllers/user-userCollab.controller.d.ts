1024c5b848f3e287dacaa0a6c3086fe1317ca4f7
nishamashhood <mashhoodnisha@gmail.com>
2025-02-23 19:18:29 +0530

    user-user connection frontend and backend

--- backend/dist/controllers/user-userCollab.controller.d.ts
+++ backend/dist/controllers/user-userCollab.controller.d.ts
@@ -3,4 +3,5 @@ export declare const sendRequestController: (req: Request, res: Response) => Pro
 export declare const respondToRequestController: (req: Request, res: Response) => Promise<void>;
 export declare const disconnectConnectionController: (req: Request, res: Response) => Promise<void>;
 export declare const getUserConnectionsController: (req: Request, res: Response) => Promise<void>;
+export declare const getUserRequestsController: (req: Request, res: Response) => Promise<void>;
 //# sourceMappingURL=user-userCollab.controller.d.ts.map
