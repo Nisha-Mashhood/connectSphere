3271a2e424a8754d77f960970240b66958ed3c26
nishamashhood <mashhoodnisha@gmail.com>
2026-01-09 12:51:12 +0530

    changed port number

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "localhost",
-    port: 5173, // Default Vite port 
-    https: false, // Change to true if testing on HTTPS
+    port: 8089, 
+    https: false,
   },
   define: { "process.env": {} },
 
